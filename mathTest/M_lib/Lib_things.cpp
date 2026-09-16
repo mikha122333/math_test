c8e7984b184ff54183095043f6411130d8583ea4
mikha122333 <mawashimisha03@gmail.com>
2026-09-16 10:26:51 +0300

    added part of class Mathtest and corrected structure Tasks

--- mathTest/M_lib/Lib_things.cpp
+++ mathTest/M_lib/Lib_things.cpp
@@ -1,1 +1,53 @@
 #include "Lib_things.h"
+Task::Task() {
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> random_num(-10000, 10000);
+	std::uniform_int_distribution<> random_operation(0, 3);//+ - / * 
+	_num_1 = random_num(gen);
+	_num_2 = random_num(gen);
+	switch (int i = random_operation(gen)) {
+	case 0: operation_ = '+'; ans_ = _num_1 + _num_2; break;
+	case 1:operation_ = '-'; ans_ = _num_1 - _num_2; break;
+	case 2:operation_ = '/';
+		while (_num_2 == 0) { _num_2 = random_num(gen); };
+		ans_ = _num_1 / _num_2;
+		break;
+	case 3:operation_ = '*'; ans_ = _num_1 * _num_2; break;
+	};
+}
+Task::Task(const double min, const double max, const char operation ) {
+	if (min > max)
+		throw std::logic_error("min>max");
+	if (operation != '+' && operation != '-' && operation != '*' && operation != '/' && operation != '\0')
+		throw std::logic_error("wrong operation");
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> random_num(min, max);
+	_num_1 = random_num(gen);
+	_num_2 = random_num(gen);
+	int op;
+	if (operation == '\0') {
+		std::uniform_int_distribution<> random_operation(0, 3);//+ - / *
+		op = random_operation(gen);
+	}
+	else {
+		if (operation == '+')
+			op = 0;
+		else if (operation == '-')
+			op = 1;
+		else if (operation == '/')
+			op = 2;
+		else if (operation == '*')
+			op = 3;
+	}
+	switch (op) {
+	case 0: operation_ = '+'; ans_ = _num_1 + _num_2; break;
+	case 1:operation_ = '-'; ans_ = _num_1 - _num_2; break;
+	case 2:operation_ = '/';
+		while (_num_2 == 0) { _num_2 = random_num(gen); };
+		ans_ = _num_1 / _num_2;
+		break;
+	case 3:operation_ = '*'; ans_ = _num_1 * _num_2; break;
+	};
+}
--- mathTest/M_lib/Lib_things.h
+++ mathTest/M_lib/Lib_things.h
@@ -2,63 +2,23 @@
 #include <random>
 #include <stdexcept>
 struct Task {
-	double num_1_;
-	double num_2_;
+	double _num_1;
+	double _num_2;
 	double ans_;
 	char operation_;
 	Task();
 	Task(const double min,const double max,const char operation='\0');
 };
-Task::Task() {
-	std::random_device rd;
-	std::mt19937 gen(rd());
-	std::uniform_int_distribution<> random_num(-10000, 10000);
-	std::uniform_int_distribution<> random_operation(0,3);//+ - / * 
-	num_1_ = random_num(gen);
-	num_2_ = random_num(gen);
-	switch (int i = random_operation(gen)) {
-	case 0: operation_ = '+'; ans_ = num_1_ + num_2_; break;
-	case 1:operation_ = '-'; ans_ = num_1_ - num_2_; break;
-		case 2:operation_ = '/';
-			while (num_2_ == 0) { num_2_ = random_num(gen); };
-			ans_ = num_1_ / num_2_;
-			break;
-		case 3:operation_ = '*'; ans_ = num_1_ * num_2_; break;
-	};
-}
-Task::Task(const double min, const double max, const char operation = '\0') {
-	if (min > max)
-		throw std::logic_error("min>max");
-	if (operation != '+' && operation != '-' && operation != '*' && operation != '/' && operation != '\0')
-		throw std::logic_error("wrong operation");
-	std::random_device rd;
-	std::mt19937 gen(rd());
-	std::uniform_int_distribution<> random_num(min, max);
-	num_1_ = random_num(gen);
-	num_2_ = random_num(gen);
-	int op;
-	if(operation=='\0'){
-		std::uniform_int_distribution<> random_operation(0, 3);//+ - / *
-		op = random_operation(gen);
-	}
-	else {
-		if (operation == '+')
-			op = 0;
-		else if (operation == '-')
-			op = 1;
-		else if (operation == '/')
-			op = 2;
-		else if (operation == '*')
-			op = 3;
-	}
-	switch (op) {
-	case 0: operation_ = '+'; ans_ = num_1_ + num_2_; break;
-	case 1:operation_ = '-'; ans_ = num_1_ - num_2_; break;
-	case 2:operation_ = '/';
-		while (num_2_ == 0) { num_2_ = random_num(gen); };
-		ans_ = num_1_ / num_2_;
-		break;
-	case 3:operation_ = '*'; ans_ = num_1_ * num_2_; break;
-	};
-}
-class MathTest {};
+class MathTest {
+	Task* _tasks;
+	size_t _count;
+	double* _usser_answers;
+	size_t _correct_count;
+public:
+	MathTest(size_t count);
+	MathTest(size_t count, int min, int max);
+	MathTest(size_t count, int min, int max, char op);
+	~MathTest();
+	void run();
+	void show_statistics()const noexcept;
+};
